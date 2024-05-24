72f2a6c145f622bd603c7b75997b2d8fe31f0d6b
SolMcCock <mastersolan@gmail.com>
2024-05-24 19:33:48 +0300

    kkk

--- Project-Corner/script.js
+++ Project-Corner/script.js
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 let docTitle = document.title;
 window.addEventListener("blur", () =>{
-    document.title = "Please come back 🥺";
+    document.title = "Please come back Siii🥺❤️";
 })
 window.addEventListener("focus", () =>{
     document.title = docTitle;
